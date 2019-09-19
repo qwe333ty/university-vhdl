6381d128d64cff95c01e851bc30a385b264327e8
Aliaksandr Rahavoi <rogovoi200023507@gmail.com>
2019-09-19 22:08:34 +0300

    lab-3 initialization

---
+++ 5th term/lab-3/test.vhd
@@ -0,0 +1,9 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Aliksandr Rahavoi
+entity test_entity is
+end test_entity;
+
+architecture test_architecture of test_entity is
+end test_architecture;
